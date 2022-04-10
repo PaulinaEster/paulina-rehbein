ab5abfb10deb86ea19f2bf8039db338ef8233679
PaulinaEster <220602eer@gmail.com>
2022-04-10 18:52:16 -0300

    a

--- App.js
+++ App.js
@@ -5,15 +5,28 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomePage from './src/pages/HomePage';
+import { Button } from 'react-native-web';
 
 const Stack = createNativeStackNavigator();
+
+const Botao = ({ navigation }) => {
+  return <View>
+    <Button title=">"  onPress={ () => navigation.navigate() }
+      color="#3E6B4A"
+    />
+  </View>
+}
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomePage} 
-          options={{ title:"HomePage" }}
+          options={{ title:"HomePage",
+          headerRight: () => {
+            return <Botao/>
+          }
+        }}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const HomePage = () => {
-  return <View>
+
+
+const HomePage = ({ navigation }) => {
+  return <View style={styles.container} >
     <Text>
-      Home Page
+      
     </Text> 
   </View> 
 }
 
 export default HomePage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#9EBAA6'
+  }
+
+})
